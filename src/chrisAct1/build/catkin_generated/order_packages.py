553dc2c887cbc7e30823036fa34c2be3011a2992
joseangeldelangel10 <joseangeldelangel10@gmail.com>
2022-04-20 23:47:00 -0500

    unecesary folder removed

--- src/chrisAct1/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/jose/Documents/6toSemestre/chrisAct1/src/chrisAct1/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/jose/Documents/6toSemestre/chrisAct1/devel;/opt/ros/melodic'.split(';') if '/home/jose/Documents/6toSemestre/chrisAct1/devel;/opt/ros/melodic' != '' else []
--- src/chrisAct1/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/jose/Documents/6toSemestre/chrisAct1/src/chrisAct1/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
