5b42ef1c5cbbfaef8c12c25d2c7283c4f83977f8
joseangeldelangel10 <joseangeldelangel10@gmail.com>
2022-04-28 17:19:38 -0500

    imageProcessor restored

--- src/chrisAct1_1/src/ImageProcessor.py
+++ src/chrisAct1_1/src/ImageProcessor.py
@@ -14,31 +14,40 @@ class ImageProcessor():
         #Inicializamos el nodo
         rospy.init_node("ImageProcessor")       
         #Creamos los susbcribers
-        self.imageSubscriber = rospy.Subscriber("/camera/image_raw/compressed",Image,self.on_image_callback)        
+        self.imageSubscriber = rospy.Subscriber("/camera/image_raw",Image,self.on_image_callback)        
         self.bridge = cv_bridge.CvBridge()        
         #Creamos los publishers
-        #self.pub_odom = rospy.Publisher("/pose2d", Pose2D, queue_size=1)        
+        self.pub_gray = rospy.Publisher("/processedImage/gray", Image, queue_size=1)        
         #Iniciamos el voctor de posición en cero
         self.image = None
         self.rate = rospy.Rate(0.1)
-        
+
+        self.cv_image = np.zeros((300,300, 300))
+        self.cv_gray_image = np.zeros((300,280))
+        self.gray_img_msg = Image()
+
     
     def on_image_callback(self, image):
-    	self.image = image.data
+        self.image = image
 
     def main(self):
-    	while not rospy.is_shutdown():
-    		if (self.image != None):
-    			self.rate.sleep()
-    			cv_image = self.bridge.imgmsg_to_cv2(self.image, desired_encoding="bgr8")
-    			cv.imshow("original img", cv_image)
-    			print("image converted succesfully")
-    			cv_gray_image = cv.cvtColor(cv_image, cv.COLOR_BGR2GRAY)
-    			
+        while not rospy.is_shutdown():
+            if (self.image != None):
+                self.rate.sleep()
+                self.cv_image = self.bridge.imgmsg_to_cv2(self.image, desired_encoding="passthrough")
+                #cv.imshow("original img", cv_image)
+                print("image converted succesfully")
+                self.cv_gray_image = cv.Canny(self.cv_image, 50, 200)
+                #cv_gray_image = None
+                #print(type(cv_gray_image))
+                #cv.imshow("gray img", cv_gray_image)
+                self.gray_img_msg = self.bridge.cv2_to_imgmsg(self.cv_gray_image, encoding = "passthrough")
+                self.pub_gray.publish(self.gray_img_msg)
+
 
 if __name__ == "__main__":
-	ip = ImageProcessor()
-	ip.main()
+    ip = ImageProcessor()
+    ip.main()
 
 
 
