4e9c74afc828535bd2142d1948fc506bd513cf55
LeoNava2202 <A01750595@tes.mx>
2022-05-04 12:19:33 -0500

    Square with traffic lights final version

--- src/chrisAct1_1/src/movePuzzlebotTrafficLights.py
+++ src/chrisAct1_1/src/movePuzzlebotTrafficLights.py
@@ -12,20 +12,16 @@ class MovePuzzlebot():
         rospy.init_node("circulo_mov_traffic_lights")
         #Creamos el publisher
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
-        self.pubTargetBool = rospy.Publisher("/arrive2Target", Bool, queue_size=1)
+        
         #Creamos los subscribers
-
         self.circleRed = rospy.Subscriber("/processedImage/detectedObjects/redCircle",Bool,self.red_circle_callback)
         self.circleGreen = rospy.Subscriber("/processedImage/detectedObjects/greenCircle",Bool,self.green_circle_callback)
         self.circleYellow = rospy.Subscriber("/processedImage/detectedObjects/yellowCircle",Bool,self.yellow_circle_callback)
 
         self.subPose = rospy.Subscriber("/pose2d",Pose2D,self.odom_callback)
 
-        self.subTarget = rospy.Subscriber("/target",Pose2D,self.target_callback)
-
         #Declaramos que vamos a mandar 20 mensajes por segundo.
         self.rate = rospy.Rate(100)
-        #Creamos el msg Twist
         self.msg = Twist()
 
         self.redCircleDetected = None
@@ -34,12 +30,14 @@ class MovePuzzlebot():
 
         self.current_pose = None
 
-        self.target = None
+        #self.target = None
+
+        self.target_index = 0
+
+        self.targets = [[2.0,0.0],[2.0,2.0],[0.0,2.0],[0.0,0.0]]
+
+        self.current_target = self.targets[self.target_index]
         
-        #self.theta_target = None
-        
-        #self.theta_reached = False
-        #self.pos_reached = False
         self.state = "travelingTowardsGoal"
         self.colorState = "Stopped"
         #Creamos un función de que hacer cuando haya un shutdown
@@ -58,9 +56,6 @@ class MovePuzzlebot():
     def odom_callback(self, odom):
         self.current_pose = odom
 
-    def target_callback(self, pose):
-        self.target = [pose.x,pose.y]
-
     def move(self,v_lin,v_ang):
         """Funcion que publica la velocidad lineal y angular en el puzzlebot"""
         #Declaramos le velocidad deseada en el mensaje tipo Twist
@@ -73,10 +68,6 @@ class MovePuzzlebot():
         #Publicamos la velocidad
         self.pub.publish(self.msg)
 
-    def sendBool(self,boolean):
-        result = Bool()
-        result.data = boolean
-        self.pubTargetBool.publish(result)
 
     def end_callback(self):
         """Funcion que para el puzzlebot cuando el nodo deja de ser corrido"""
@@ -86,35 +77,64 @@ class MovePuzzlebot():
         #Publicamos las velocidades
         self.pub.publish(self.msg)
 #Si el archivo es corrido directametne y no llamado desde otro archivo corremos
+
+
 if __name__ == "__main__":
     #iniciamos la clase
     mov = MovePuzzlebot()
     #mientras este corriendo el nodo movemos el carro el circulo
-    Kpw = 0.92
+    Kpw = 1.0
     Kpv = 0.25
     while not rospy.is_shutdown():
         #Llamamos el sleep para asegurar los 20 msg por segundo
         
-        if mov.current_pose != None and mov.target != None:
+        if mov.current_pose != None:
             #print("None condition passed")
+ 
 
-            theta_target = np.arctan2(mov.target[1]-mov.current_pose.y, mov.target[0]-mov.current_pose.x)
+            theta_target = np.arctan2(mov.current_target[1]-mov.current_pose.y, mov.current_target[0]-mov.current_pose.x)
             e_theta = theta_target - mov.current_pose.theta
-            e_pos = np.sqrt((mov.target[0]-mov.current_pose.x)**2 + (mov.target[1]-mov.current_pose.y)**2)
+            e_pos = np.sqrt((mov.current_target[0]-mov.current_pose.x)**2 + (mov.current_target[1]-mov.current_pose.y)**2)
             
+            if mov.state == "pointingTowardsGoal":
+                w = Kpw*e_theta
+                v = 0.0
+
+                           
+                if w >= 0.25:
+                    w = 0.24
+                elif w <= -0.25:
+                    w = -0.24
+                
+
+                if (mov.redCircleDetected == True):
+                    mov.colorState = "Stopped"
+                if (mov.yellowCircleDetected == True):
+                    mov.colorState = "Running Half"
+                if (mov.greenCircleDetected == True):
+                    mov.colorState = "Running"
+
+                if (mov.colorState == "Running Half"):
+                    w = w/2
+                elif (mov.colorState == "Stopped"):
+                    w = 0.0                
+                
+                mov.move(v,w)
+
+
             if mov.state == "travelingTowardsGoal":
                 w = Kpw*e_theta
                 v = Kpv*e_pos
 
                            
-                if w >= 0.2:
-                    w = 0.19
-                elif w <= -0.2:
-                    w = -0.19
-                if v >= 0.2:                   
-                    v = 0.19                                        
-                elif v <= -0.2:                    
-                    v = -0.19
+                if w >= 0.25:
+                    w = 0.24
+                elif w <= -0.25:
+                    w = -0.24
+                if v >= 0.25:                   
+                    v = 0.24                                        
+                elif v <= -0.25:                    
+                    v = -0.24
 
                 if (mov.redCircleDetected == True):
                     mov.colorState = "Stopped"
@@ -131,25 +151,29 @@ if __name__ == "__main__":
                     w = 0.0                
                 
                 mov.move(v,w)
-                mov.sendBool(False)
+        
+            if (abs(e_theta) <= 0.025) and mov.state == "pointingTowardsGoal":
+
+                mov.state = "travelingTowardsGoal"
                 
 
                 
             if (abs(e_theta) <= 0.025) and (abs(e_pos) <= 0.02 and mov.state == "travelingTowardsGoal"):
                 mov.move(0.0,0.0)
-                mov.state = "goalReached"
-                print("============ final pose based in odometry is: =============")
+                #mov.state = "goalReached"
+                print("============ target {target_num} reached, pose based in odometry is: =============".format(target_num = mov.target_index))
                 print(mov.current_pose)
-                mov.sendBool(True)
+                if mov.target_index == len(mov.targets)-1:
+                    mov.state = "goalReached"
+                else:                    
+                    mov.target_index += 1
+                    mov.current_target = mov.targets[mov.target_index]
+                    mov.state = "pointingTowardsGoal"
+                
 
             if mov.state == "goalReached":
                 mov.move(0.0,0.0)
-                mov.sendBool(False) 
-
-            if (abs(e_theta) >= 0.025) and (abs(e_pos) >= 0.02 and mov.state == "goalReached"):
-                print("NEW TARGET RECIEVED AND STATE CHANGED")
-                mov.state = "travelingTowardsGoal"
-                mov.sendBool(False)
+                
 
 
             
